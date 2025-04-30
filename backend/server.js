6b57e7e3129689e0ff5d3431c3e2b4d1fd58fd3a
jsills2 <jsills2592@gmail..com>
2025-04-30 10:11:19 -0400

    added urlencoded to true

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,9 @@ const exerciseRouter = require('./routes/exercises');
 
 const app = express();
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
 
 
 
